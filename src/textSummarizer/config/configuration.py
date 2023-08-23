a91e587170da26138d4180853d5c9915629bc3ec
Rahul Jain <rahuljain809@gmail.com>
2023-08-22 17:11:01 -0700

    validation pipeline completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.logging import logger
-from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -10,7 +10,6 @@ class ConfigurationManager:
         params_filepath = PARAMS_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
-        logger.info(self.config)
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
@@ -30,3 +29,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
