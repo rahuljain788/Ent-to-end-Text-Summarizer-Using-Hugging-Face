1d904fe4d211bebf44ef30c9347ed431dd3ceff7
Rahul Jain <rahuljain809@gmail.com>
2023-08-22 20:25:27 -0700

    model trainer added

---
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,16 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.conponents.model_trainer import ModelTrainer
+from textSummarizer.logging import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            model_trainer_config = config.get_model_trainer_config()
+            model_trainer_config = ModelTrainer(config=model_trainer_config)
+            model_trainer_config.train()
+        except Exception as e:
+            raise e
